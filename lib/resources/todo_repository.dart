f50a6faabe9180af2123d7bded36aeaf99424f19
samfan <samfan@live.com>
2020-08-10 02:03:06 +0530

    data source class updation

--- lib/resources/todo_repository.dart
+++ lib/resources/todo_repository.dart
@@ -1,5 +1,11 @@
+import 'package:todo/models/todo_model.dart';
+
 class TodoRepository {}
 
-abstract class TodoSource {}
+abstract class TodoSource {
+  Future<List<TodoModel>> getTodos();
+  Future<bool> updateTodo(int id);
+  Future<bool> deleteTodo(int id);
+}
 
 abstract class TodoCache {}
